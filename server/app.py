146bde0879ca24b910045bd4a5c9d2d65d76b50e
george <gelkik@gmail.com>
2023-03-21 11:30:09 -0400

    final

--- server/app.py
+++ server/app.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python3
 
-from flask import Flask, make_response
+from flask import Flask, make_response, request
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
 
-from models import db, Hero
+from models import db, Hero, Power, HeroPower
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
+
 
 migrate = Migrate(app, db)
 
@@ -19,8 +20,137 @@ api = Api(app)
 
 @app.route('/')
 def home():
-    return ''
+    response_body = {
+        'Welcome to the Hero Database!'
+    }
+    response = make_response(
+        response_body,
+        200
+    )
+    return response
 
+@app.route('/heroes',methods = ['GET'])
+def heroes():
+
+    heroes = Hero.query.all()
+
+    if request.method == 'GET':
+        heroes_dict = [heroes.to_dict() for heroes in heroes]
+        
+        response = make_response(
+            heroes_dict,
+            200
+        )
+
+        return response
+
+@app.route('/heroes/<int:id>',methods = ['GET'])
+def heroes_id(id):
+    hero = Hero.query.filter(Hero.id == id).first()
+
+    if not hero:
+        response_body = {
+            "error": "Hero not found"
+        }
+        response = make_response(
+            response_body,
+            404
+        )
+        return response
+
+    elif request.method == 'GET':
+        hero_dict = hero.to_dict()
+        
+        response = make_response(
+            hero_dict,
+            200
+        )
+
+        return response
+    
+@app.route('/powers',methods = ['GET'])
+def powers():
+    powers = Power.query.all()
+
+    if request.method == 'GET':
+        power_dict = [powers.to_dict() for powers in powers]
+        
+        response = make_response(
+            power_dict,
+            200
+        )
+
+        return response
+    
+@app.route('/powers/<int:id>',methods = ['GET','PATCH'])
+def power_id(id):
+    power = Power.query.filter(Power.id == id).first()
+
+    if not power:
+        response_body = {
+            "error": "Power not found"
+        }
+        response = make_response(
+            response_body,
+            404
+        )
+        return response
+
+    elif request.method == 'GET':
+        power_dict = power.to_dict()
+        
+        response = make_response(
+            power_dict,
+            200
+        )
+
+        return response
+    
+    elif request.method == 'PATCH':
+        for attr in request.get_json():
+            setattr(power,attr,request.get_json()[attr])
+        db.session.add(power)
+        db.session.commit()
+
+        power_dict = power.to_dict()
+        
+        response = make_response(
+            power_dict,
+            200
+        )
+
+        return response
+    
+@app.route('/hero_powers',methods=['GET','POST'])
+def hero_powers():
+
+    if request.method == 'GET':
+        hero_powers = Hero.query.all()
+        hero_dict = [hero.to_dict() for hero in hero_powers]
+        response = make_response(
+            hero_dict,
+            200
+        )
+        return response
+
+    elif request.method == 'POST':
+        new_hero_power = HeroPower(
+            strength = request.get_json()['strength'],
+            power_id = request.get_json()['power_id'],
+            hero_id = request.get_json()['hero_id']
+        )
+        db.session.add(new_hero_power)
+        db.session.commit()
+
+        hero_power_dict = new_hero_power.to_dict()
+
+        response = make_response(
+            jsonify(hero_power_dict),
+            201
+        )
+
+
+        return response
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -1,10 +1,67 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
+from sqlalchemy_serializer import SerializerMixin
+from sqlalchemy.orm import validates
+from sqlalchemy.ext.associationproxy import association_proxy
 
-db = SQLAlchemy()
+metadata = MetaData(naming_convention={
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+})
 
-class Hero(db.Model):
-    __tablename__ = 'hero'
+db = SQLAlchemy(metadata=metadata)
+
+
+class Hero(db.Model,SerializerMixin):
+    __tablename__ = 'heroes'
+
+    serialize_rules = ('-hero_powers','-created_at','-updated_at','-powers.heroes')
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    super_name = db.Column(db.String)
+    created_at = db.Column(db.DateTime, server_default = db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate = db.func.now())
 
-# add any models you may need. 
+    hero_powers = db.relationship('HeroPower',backref = 'hero')
+    powers = association_proxy('hero_powers','power')
+
+class Power(db.Model,SerializerMixin):
+    __tablename__ = 'powers'
+
+    serialize_rules = ('-hero_powers','-created_at','-updated_at','-heroes.created_at','-heroes.updated_at','-heroes.powers')
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    description = db.Column(db.String)
+    created_at = db.Column(db.DateTime, server_default = db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate = db.func.now())
+
+    hero_powers = db.relationship('HeroPower',backref = 'power')
+    heroes = association_proxy('hero_powers','hero')
+
+    @validates('description')
+    def validate_description(self,key,description):
+        if not description or type(description) != str or len(description) < 20:
+            raise ValueError('Description not valid.')
+        return description
+
+class HeroPower(db.Model,SerializerMixin):
+    __tablename__ = 'hero_powers'
+
+    serialize_rules = ('-hero.hero_powers','-power.hero_powers','-created_at','-updated_at','-power.heroes','-hero.powers')
+
+    id = db.Column(db.Integer, primary_key=True)
+    strength = db.Column(db.String)
+    created_at = db.Column(db.DateTime, server_default = db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate = db.func.now())
+
+    hero_id = db.Column(db.Integer,db.ForeignKey('heroes.id'))
+    power_id = db.Column(db.Integer,db.ForeignKey('powers.id'))
+
+    @validates('strength')
+    def validate_strength(self,key,strength):
+        category = ['Strong', 'Weak', 'Average']
+        if strength not in category:
+            raise ValueError('Invalid. Strength must be one of: Strong, Weak, Average')
+        return strength
+
